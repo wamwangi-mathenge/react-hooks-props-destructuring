07cbc4084a3f279ba67749d187176791d2571147
Brian <wamwangimathenge@gmail.com>
2023-01-12 09:06:58 +0300

    Mad Max Poster Added

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,8 @@ function App() {
   return (
     <div className="App">
       <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={title} posterSrc= {posterURL} genres={genresArr} />
+      <MovieCard title={title} posterSrc= {posterURL} genres={genresArr} />
     </div>
   );
 }
